bb7ad1650eceb6d70ba3fea2c3a69c74b2e1d45e
darinasimeonova <darrinasimeonova84@gmail.com>
2023-02-17 11:27:38 +0200

    new commit

--- MusicShopWebApp/MusicShopWebApp/Data/ApplicationDbContext.cs
+++ MusicShopWebApp/MusicShopWebApp/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace MusicShopWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
